a421ecc4a7b253da4574e30ce870dada6debbd5c
AVOHOU Prince Boris <avohouprince@gmail.com>
2024-10-29 15:38:48 +0100

    first

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
   has_many :orders, dependent: :destroy
+
+  def orders_count
+    orders.count
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
   has_many :addresses, as: :addressable, dependent: :destroy
   has_many :orders, dependent: :destroy
+
+  def foods_price_sum
+    orders.joins(:order_foods).sum('order_foods.price')
+  end
 end
